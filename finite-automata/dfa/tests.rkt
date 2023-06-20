309e1ea15fc40f4f5f3d7ce13e0016f27fe381ec
costascaio <caio.soares@aluno.ufop.edu.br>
2023-06-19 21:14:46 -0300

    refactor: add new file to represent brzozowski algorithm

--- finite-automata/dfa/tests.rkt
+++ finite-automata/dfa/tests.rkt
@@ -5,7 +5,7 @@
          "image-builder.rkt"
          "../nfa/image-builder.rkt"
          "../nfa/subset-construction.rkt"
-         "table-minimization.rkt"
+         "brzozowski-minimization.rkt"
          "../../utils/dot.rkt")
 
 
@@ -26,36 +26,4 @@
        (B : 0 -> B)
        (B : 1 -> B)))
 
-;; (define reverse (revert-delta (dfa-delta A)))
-
-;; (revert-delta (dfa-delta A))
-
 (minimization T)
-
-; function that group all the transitions that has the same beginning state and same symbol
-(define (test delta)
-  (remove-duplicates
-   (map (lambda (t1)
-         (filter (lambda (t2)
-                (if (and (eqv? (car (car t1)) (car (car t2)))
-                     (eqv? (cdr (car t1)) (cdr (car t2))))
-                    (cons (cons (car (car t2)) (cdr (car t2))) (cdr t2))
-                    #f
-                    )) delta)) delta )))
-
-; (test reverse)
-; generate a list of final states of each grouped transition 
-(define (solution grouped-transitions)
-  (map (lambda (transitions)
-         (map (lambda (single-transition)
-                (cdr single-transition)) transitions)) grouped-transitions))
-
-(define (concat-grouped-transitions grouped-transitions destiny-states)
-  (map (lambda (group)
-         (cons (car (first group)) (list (list-ref destiny-states (index-of grouped-transitions group))))) grouped-transitions))
-
-;; (define s (solution (test reverse)))
-;; (concat-grouped-transitions (test reverse) (solution (test reverse)) )
-;(dfa->pict T)
-;(dfa->pict (minimize T))
-;(dfa->pict (minimization T))
